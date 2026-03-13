5b8990c4c9795e1c9fda4fcdbf3e6664a30ff393
DatLNT <tle723772@gmail.com>
2026-03-13 23:26:46 +0700

    First commit project QuanLyBanVeMayBay

---
+++ src/main/java/com/example/quanlybanvemaybay/QuanLyBanVeMayBayApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlybanvemaybay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBanVeMayBayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyBanVeMayBayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quanlybanvemaybay/QuanLyBanVeMayBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlybanvemaybay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanVeMayBayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
